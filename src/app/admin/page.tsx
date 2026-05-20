115562940bd1f4b3efebc3a4e5259c0c36272c9f
thakrons-cloud <thakron.s@gmail.com>
2026-05-20 11:29:47 +0700

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -303,7 +303,7 @@ export default function AdminDashboard() {
           </motion.div>
         )}
 
-        {(!eventLoading && !teamsLoading && activeTeam) ? (
+        {(!eventLoading && !teamsLoading) ? (
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           {/* Main Control Panel */}
           <div className="lg:col-span-2 glass-panel p-6 md:p-8 flex flex-col">
